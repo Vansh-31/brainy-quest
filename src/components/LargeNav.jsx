bce1cae553602ba83bf4b813cac391bf658280d8
Vansh Gawra <vanshgawra008@gmail.com>
2023-10-03 04:33:12 +0530

    Completed LargeNav and added DarkMode Slice

--- src/components/LargeNav.jsx
+++ src/components/LargeNav.jsx
@@ -1,21 +1,48 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import logo from '../assets/logo.png'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+import logo from "../assets/logo.png";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleDarkMode } from "../redux/slices/darkModeSlice";
 
 const LargeNav = () => {
-  return (
-    <div className='w-full min-h-[12vh] h-20 bg-primary' >
-      <nav className='w-10/12 h-full max-w-6xl mx-auto flex justify-between items-center' >
-      <NavLink to="/" className='h-full p-2 flex gap-x-2 justify-center items-center' >
-        <img className='h-full'  src={logo} alt="" />
-        <div>
-          <p className='font-bold text-white text-xl'>Brainy Quest</p>
-          <p className='text-white' >Test Your Brain</p>
-        </div>
-      </NavLink>
-      </nav>
-    </div>
-  )
-}
+	const dispatch = useDispatch();
+	const darkMode = useSelector((state) => state.darkMode);
+	return (
+		<div className="w-full min-h-[12vh] h-20 bg-secondary">
+			<nav className="w-10/12 h-full max-w-6xl mx-auto flex justify-between items-center">
+				<NavLink
+					to="/"
+					className="h-full p-2 flex gap-x-2 justify-center items-center"
+				>
+					<img className="h-full" src={logo} alt="" />
+					<div>
+						<p className="font-bold text-white text-xl">Brainy Quest</p>
+						<p className="text-white">Test Your Brain</p>
+					</div>
+				</NavLink>
+				<div className="h-full p-2 flex justify-center items-center text-white font-semibold text-lg overflow-hidden">
+					<NavLink className="px-4 py-10 hover:bg-primary" to="/">
+						Home
+					</NavLink>
+					<NavLink className="px-4 py-10 hover:bg-primary" to="/">
+						Take a QUIZ
+					</NavLink>
+					<NavLink className="px-4 py-10 hover:bg-primary" to="/">
+						Results
+					</NavLink>
+					<button
+						className="mx-4 text-2xl"
+						onClick={() => {
+							dispatch(toggleDarkMode());
+						}}
+					>
+						{darkMode ? <BsSunFill></BsSunFill> : <BsMoonFill></BsMoonFill>}
+					</button>
+				</div>
+			</nav>
+		</div>
+	);
+};
 
-export default LargeNav
+export default LargeNav;
