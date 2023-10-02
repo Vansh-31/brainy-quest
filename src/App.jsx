0a71f5abc2461a3d2b9123b3c37e9aaacbc5c355
Vansh Gawra <vanshgawra008@gmail.com>
2023-10-03 03:13:15 +0530

    Added Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import LargeNav from "./components/LargeNav";
+import Home from "./pages/Home";
 
 function App() {
-	return <div>APP</div>;
+	return (
+		<div>
+			<Routes>
+				<Route path="/" element={LargeNav}>
+					<Route index element={<Home></Home>}></Route>
+				</Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
