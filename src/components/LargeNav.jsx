69b913fe680fa1e9626c9be9e80179cdef5bb13b
Vansh Gawra <vanshgawra008@gmail.com>
2023-10-03 04:08:56 +0530

    Added Logo to NavBar

--- src/components/LargeNav.jsx
+++ src/components/LargeNav.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import logo from '../assets/logo.png'
 
 const LargeNav = () => {
   return (
-    <div>
-      Large Nav
+    <div className='w-full min-h-[12vh] h-20 bg-primary' >
+      <nav className='w-10/12 h-full max-w-6xl mx-auto flex justify-between items-center' >
+      <NavLink to="/" className='h-full p-2 flex gap-x-2 justify-center items-center' >
+        <img className='h-full'  src={logo} alt="" />
+        <div>
+          <p className='font-bold text-white text-xl'>Brainy Quest</p>
+          <p className='text-white' >Test Your Brain</p>
+        </div>
+      </NavLink>
+      </nav>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(var(--color-primary) / 1)",
+        secondary: "rgb(var(--color-secondary) / 1)",
+        primaryDark: "rgb(var(--color-primary-dark) / 1)",
+      },
+    },
   },
   plugins: [],
-}
+};
